1b5b156d1ca63bcca3ab0bffc059436a2ae81930
Erick Hernandez <yerickk8@gmail.com>
2022-06-17 13:57:45 -0500

    first commit

---
+++ compressor.py
@@ -0,0 +1,13 @@
+from PIL import Image # pip install Pillow
+
+import os
+
+downloadsFolder = "/Users/yeric/Downloads/" #Insert the folder where you store the images 
+
+if __name__ == "__main__":
+    for filename in os.listdir(downloadsFolder):
+        name, extension = os.path.splitext(downloadsFolder + filename)
+
+        if extension in [".jpg", ".jpeg", ".png"]:
+            picture = Image.open(downloadsFolder + filename)
+            picture.save(downloadsFolder + "compressed_"+filename, optimize=True, quality=60)
